8e679b251920e6e4cd20f505b61c4b14f7683016
Seu Nome <arthurbuscarinobenedetti8@gmail.com>
2026-03-17 19:13:03 -0300

    integração entre os arquivos

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from processamento import processar_alunos
+
 def main():
-    print("Sistema de análise de desempenho iniciado")
+    alunos = [
+        ("Arthur", [8, 7, 9]),
+        ("Maria", [5, 6, 5]),
+        ("João", [])
+    ]
+
+    processar_alunos(alunos)
 
 if __name__ == "__main__":
     main()
